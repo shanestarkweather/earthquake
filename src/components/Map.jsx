a853fcc2c7699a072a39061ef8fa68f38f38b415
shanestarkweather <shane@shanestarkweather.com>
2020-10-30 16:51:40 -0700

    Details map working. Needs pin.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,23 +1,32 @@
 import React from 'react'
 import GoogleMapReact from 'google-map-react'
 import './Map.css'
-import EventDetails from './EventDetails'
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 
-const Map = ({ earthquake }) => (
-  <div className="map">
+const Map = ({ earthquake }) => {
+  if (!earthquake.latitude) {
+  return null
+  }
+  return <div className="map">
     <h2 className="map-h2">{earthquake.title}</h2>
 
     <div className="google-map">
       <GoogleMapReact
         bootstrapURLKeys={{ key: process.env.REACT_APP_GMAP_API_KEY }}
-        defaultCenter={{ lat: 39.8283, lng: 98.5795 }}
-        defaultZoom={6}
+        center={{ lat: parseInt(earthquake.latitude), lng: parseInt(earthquake.longitude) }}
+        defaultZoom={10}
       >
-       
+        <AnyReactComponent
+            lat={parseInt(earthquake.latitude)}
+            lng={parseInt(earthquake.longitude)}
+            text={earthquake.title}
+          />
       </GoogleMapReact>
     </div>
   </div>
-)
+}
+
 
 export default Map
