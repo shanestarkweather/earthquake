364a6ece15d4f6bc1c1a4db7a82c4002b3f4757b
shanestarkweather <shane@shanestarkweather.com>
2020-11-02 05:56:45 -0800

    Ready to submit for P2

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,15 @@ const Header = ({setTime, setMagnitude}) => {
 	return (
 		<div className='header'>
 			<form>
-				<select onChange={ (event) => setTime(event.target.value)}>
+				<h3>Time frame:</h3>
+				<select onChange={(event) => setTime(event.target.value)}>
 					<option value='hour'>Past Hour</option>
 					<option value='day'>Past Day</option>
 					<option value='week'>Past 7 Days</option>
 					<option value='month'>Past 30 Days</option>
 				</select>
-				<select onChange={ (event) => setMagnitude(event.target.value)}>
+				<h3>Magnitude:</h3>
+				<select onChange={(event) => setMagnitude(event.target.value)}>
 					<option value='1.0'>1.0+</option>
 					<option value='2.5'>2.5+</option>
 					<option value='4.5'>4.5+</option>
